a3c0ba802b98408cf5012487406b81988bea5a4c
Jaryl Sim <jaryl.sim@me.com>
2022-09-03 09:54:02 +0800

    Spruce up login and create account screens.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def input_class(error: false)
+    base = "block w-full appearance-none rounded-md border sm:text-sm"
+    if error
+      "#{base} border-red-300 pr-10 text-red-900 placeholder-red-300 focus:border-red-500 focus:outline-none focus:ring-red-500"
+    else
+      "#{base} border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+    end
+  end
 end
