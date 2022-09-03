48142cfa2c441148fbdd141340653f4efdea1e43
Jaryl Sim <jaryl.sim@me.com>
2022-09-03 09:57:02 +0800

    Replace all indigo colours with green.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     if error
       "#{base} border-red-300 pr-10 text-red-900 placeholder-red-300 focus:border-red-500 focus:outline-none focus:ring-red-500"
     else
-      "#{base} border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+      "#{base} border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500"
     end
   end
 end
